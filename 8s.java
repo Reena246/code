bbb715095807dca4bcea433a62a73d0777874375
Reena246 <vrasagna7@gmail.com>
2026-01-04 22:45:23 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,10 +1,1 @@
-server.error.include-message=always
-server.error.include-binding-errors=always
-server.error.include-stacktrace=always
-
-spring.jpa.show-sql=true
-spring.jpa.properties.hibernate.format_sql=true
-
-logging.level.org.springframework.web=DEBUG
-logging.level.org.hibernate.SQL=DEBUG
-logging.level.org.hibernate.type.descriptor.sql=TRACE
+///
