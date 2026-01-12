b31fbe7655edc1988bb00a004cfb5c2e5ea8f072
Reena246 <vrasagna7@gmail.com>
2026-01-11 20:34:53 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,52 +1,9 @@
 {
-    "version": "2.0.0",
-    "tasks": [
-        {
-            "label": "maven: clean install",
-            "type": "shell",
-            "command": "mvn",
-            "args": [
-                "clean",
-                "install",
-                "-DskipTests"
-            ],
-            "group": {
-                "kind": "build",
-                "isDefault": true
-            },
-            "presentation": {
-                "reveal": "always",
-                "panel": "shared"
-            },
-            "problemMatcher": []
-        },
-        {
-            "label": "maven: spring-boot:run",
-            "type": "shell",
-            "command": "mvn",
-            "args": [
-                "spring-boot:run"
-            ],
-            "group": "build",
-            "presentation": {
-                "reveal": "always",
-                "panel": "shared"
-            },
-            "problemMatcher": []
-        },
-        {
-            "label": "maven: test",
-            "type": "shell",
-            "command": "mvn",
-            "args": [
-                "test"
-            ],
-            "group": "test",
-            "presentation": {
-                "reveal": "always",
-                "panel": "shared"
-            },
-            "problemMatcher": []
-        }
+    "recommendations": [
+        "vscjava.vscode-java-pack",
+        "vscjava.vscode-spring-boot-dashboard",
+        "vscjava.vscode-spring-boot",
+        "vscjava.vscode-maven",
+        "vmware.vscode-spring-boot"
     ]
 }
