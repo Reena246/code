c934449f03b629d7ccf013eb72483e96091d83d2
Reena246 <vrasagna7@gmail.com>
2026-01-11 19:04:01 -0600

    Refactor AccessCardRepository to AuditRepository

--- 8s.java
+++ 8s.java
@@ -1,13 +1,15 @@
 package com.company.badgemate.repository;
 
-import com.company.badgemate.entity.AccessCard;
+import com.company.badgemate.entity.Audit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
-public interface AccessCardRepository extends JpaRepository<AccessCard, Long> {
-    Optional<AccessCard> findByCardUid(String cardUid);
-    Optional<AccessCard> findByCardNumber(String cardNumber);
+public interface AuditRepository extends JpaRepository<Audit, Long> {
+    List<Audit> findByEventTimeBetween(LocalDateTime start, LocalDateTime end);
+    List<Audit> findByCardId(Long cardId);
+    List<Audit> findByDoorId(Long doorId);
 }
