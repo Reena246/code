5fb6d1e40b154600a0cedbeeb7fb58f12ddb4756
Reena246 <vrasagna7@gmail.com>
2026-01-11 18:56:08 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,12 +1,9 @@
 package com.company.badgemate.repository;
 
-import com.company.badgemate.entity.DoorLock;
+import com.company.badgemate.entity.Reader;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface DoorLockRepository extends JpaRepository<DoorLock, Long> {
-    Optional<DoorLock> findByDoorId(Long doorId);
+public interface ReaderRepository extends JpaRepository<Reader, Long> {
 }
