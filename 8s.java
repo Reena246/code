e0eb6dc7329f0f0f583851ae50dcd784a16c9f12
Reena246 <vrasagna7@gmail.com>
2026-01-11 18:58:50 -0600

    Add AccessGroupDoorRepository interface

--- 8s.java
+++ 8s.java
@@ -1,9 +1,10 @@
 package com.company.badgemate.repository;
 
-import com.company.badgemate.entity.AccessGroup;
+import com.company.badgemate.entity.AccessGroupDoor;
+import com.badgemate.entity.AccessGroupDoorId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccessGroupRepository extends JpaRepository<AccessGroup, Long> {
+public interface AccessGroupDoorRepository extends JpaRepository<AccessGroupDoor, AccessGroupDoorId> {
 }
