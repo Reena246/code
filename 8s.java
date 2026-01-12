b47457afc1175836d5a3c8afee0c1320ad8fdfe1
Reena246 <vrasagna7@gmail.com>
2026-01-11 19:02:32 -0600

    Create AccessCardRepository for AccessCard entity

--- 8s.java
+++ 8s.java
@@ -1,12 +1,13 @@
 package com.company.badgemate.repository;
 
-import com.company.badgemate.entity.Employee;
+import com.company.badgemate.entity.AccessCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Optional<Employee> findByEmployeeCode(String employeeCode);
+public interface AccessCardRepository extends JpaRepository<AccessCard, Long> {
+    Optional<AccessCard> findByCardUid(String cardUid);
+    Optional<AccessCard> findByCardNumber(String cardNumber);
 }
