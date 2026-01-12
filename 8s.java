ef1546f1db457807b7e9c4f28c7cdb220909b2e9
Reena246 <vrasagna7@gmail.com>
2026-01-11 20:32:51 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,27 +1,24 @@
-package com.company.badgemate.config;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SwaggerConfig {
-    
-    @Bean
-    public OpenAPI badgeMateOpenAPI() {
-        return new OpenAPI()
-            .info(new Info()
-                .title("BadgeMate Access Control System API")
-                .description("REST API for BadgeMate Access Control System with MQTT integration")
-                .version("1.0.0")
-                .contact(new Contact()
-                    .name("BadgeMate Team")
-                    .email("support@badgemate.com"))
-                .license(new License()
-                    .name("Apache 2.0")
-                    .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
-    }
+{
+    "java.configuration.updateBuildConfiguration": "automatic",
+    "java.compile.nullAnalysis.mode": "automatic",
+    "java.configuration.runtimes": [
+        {
+            "name": "JavaSE-21",
+            "path": "/Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home",
+            "default": true
+        }
+    ],
+    "files.exclude": {
+        "**/.classpath": true,
+        "**/.project": true,
+        "**/.settings": true,
+        "**/.factorypath": true
+    },
+    "java.project.sourcePaths": [
+        "src/main/java"
+    ],
+    "java.project.outputPath": "target/classes",
+    "java.project.referencedLibraries": [
+        "target/**/*.jar"
+    ]
 }
