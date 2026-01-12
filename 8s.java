216a9bf0e23b53d63bb4d3c3bf79c912642a4771
Reena246 <vrasagna7@gmail.com>
2026-01-11 18:31:52 -0600

    Refactor AccessGroupDoor class into AccessGroupDoorId

--- 8s.java
+++ 8s.java
@@ -1,59 +1,15 @@
 package com.company.badgemate.entity;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.io.Serializable;
 
-@Entity
-@Table(name = "access_group_door")
-@IdClass(AccessGroupDoorId.class)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AccessGroupDoor {
-    
-    @Id
-    @Column(name = "access_group_id", nullable = false)
+public class AccessGroupDoorId implements Serializable {
     private Long accessGroupId;
-    
-    @Id
-    @Column(name = "door_id", nullable = false)
     private Long doorId;
-    
-    @Enumerated(EnumType.STRING)
-    @Column(name = "access_type", nullable = false)
-    private AccessType accessType;
-    
-    @Column(name = "is_active", nullable = false)
-    private Boolean isActive = true;
-    
-    @Column(name = "created", nullable = false, updatable = false)
-    private LocalDateTime created;
-    
-    @Column(name = "updated")
-    private LocalDateTime updated;
-    
-    @Column(name = "created_by", length = 20)
-    private String createdBy;
-    
-    @Column(name = "updated_by", length = 20)
-    private String updatedBy;
-    
-    @PrePersist
-    protected void onCreate() {
-        created = LocalDateTime.now();
-        updated = LocalDateTime.now();
-    }
-    
-    @PreUpdate
-    protected void onUpdate() {
-        updated = LocalDateTime.now();
-    }
-    
-    public enum AccessType {
-        ALLOW, DENY
-    }
 }
