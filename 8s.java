e4a9ce49a686ee663f2d5728cf727e045fbfc530
Reena246 <vrasagna7@gmail.com>
2026-01-12 21:17:45 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,9 +1,6 @@
 package com.demo.accesscontrolsystem.repository;
 
-import com.demo.accesscontrolsystem.entity.AccessCard;
+import com.demo.accesscontrolsystem.entity.Audit;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
-public interface AccessCardRepository extends JpaRepository<AccessCard, Long> {
-    Optional<AccessCard> findByCardUidAndIsActive(String cardUid, Boolean isActive);
-}
+public interface AuditRepository extends JpaRepository<Audit, Long> {}
