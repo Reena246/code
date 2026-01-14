4eb74aeabcbe17bba7b6e8cc9f887a5c8e7f29a4
Reena246 <vrasagna7@gmail.com>
2026-01-13 22:44:08 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,13 +1,9 @@
 package com.project.badgemate.repository;
 
-import com.project.badgemate.entity.AccessGroupDoor;
+import com.project.badgemate.entity.Audit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface AccessGroupDoorRepository extends JpaRepository<AccessGroupDoor, AccessGroupDoor.AccessGroupDoorId> {
-    List<AccessGroupDoor> findByAccessGroupIdAndDoorIdAndIsActiveTrue(Long accessGroupId, Long doorId);
-    List<AccessGroupDoor> findByAccessGroupIdAndIsActiveTrue(Long accessGroupId);
+public interface AuditRepository extends JpaRepository<Audit, Long> {
 }
