701101ab3e6ec07a546c740723850d5ff68da203
Reena246 <vrasagna7@gmail.com>
2026-01-13 22:47:32 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,12 +1,12 @@
 package com.project.badgemate.repository;
 
-import com.project.badgemate.entity.Employee;
+import com.project.badgemate.entity.Reader;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Optional<Employee> findByEmployeePkAndIsActiveTrue(Long employeePk);
+public interface ReaderRepository extends JpaRepository<Reader, Long> {
+    Optional<Reader> findByDoorIdAndIsActiveTrue(Long doorId);
 }
