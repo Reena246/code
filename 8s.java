f8b2dbd20eb54773c32c63dee00f7b717dfa4957
Reena246 <vrasagna7@gmail.com>
2026-01-19 17:42:10 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BadgemateApplication {
+public class AccessControlApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BadgemateApplication.class, args);
+        SpringApplication.run(AccessControlApplication.class, args);
     }
 }
