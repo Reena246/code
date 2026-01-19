1c35f86b4a8ad695b4955cdf08460a0e248faf10
Reena246 <vrasagna7@gmail.com>
2026-01-19 17:57:10 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,49 +1,13 @@
-package com.company.badgemate.entity;
+package com.company.badgemate.repository;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import java.time.LocalDateTime;
+import com.company.badgemate.entity.AccessCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-@Entity
-@Table(name = "site")
-@Data
-public class Site {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "site_id")
-    private Long siteId;
-    
-    @Column(name = "company_id")
-    private Long companyId;
-    
-    @Column(name = "site_name", length = 80)
-    private String siteName;
-    
-    @Column(name = "address_line1", length = 120)
-    private String addressLine1;
-    
-    @Column(name = "city", length = 60)
-    private String city;
-    
-    @Column(name = "state", length = 40)
-    private String state;
-    
-    @Column(name = "country", length = 40)
-    private String country;
-    
-    @Column(name = "is_active")
-    private Boolean isActive;
-    
-    @Column(name = "created")
-    private LocalDateTime created;
-    
-    @Column(name = "updated")
-    private LocalDateTime updated;
-    
-    @Column(name = "created_by", length = 20)
-    private String createdBy;
-    
-    @Column(name = "updated_by", length = 20)
-    private String updatedBy;
+@Repository
+public interface AccessCardRepository extends JpaRepository<AccessCard, Long> {
+    Optional<AccessCard> findByCardUidAndIsActiveTrue(String cardUid);
+    Optional<AccessCard> findByCardNumberAndIsActiveTrue(String cardNumber);
+    Optional<AccessCard> findByEmployeePkAndIsActiveTrue(Long employeePk);
 }
