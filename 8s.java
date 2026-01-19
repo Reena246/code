3f70aa4848d970fa50ea98d1cc4ed6ec963bc46e
Reena246 <vrasagna7@gmail.com>
2026-01-19 14:44:31 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,24 +1,47 @@
-package com.project.badgemate.dto;
+package com.project.badgemate.service;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.project.badgemate.dto.ServerHeartbeat;
+import com.project.badgemate.dto.ServerHeartbeatResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class ServerHeartbeatResponse {
+import java.time.Instant;
+
+@Service
+public class ServerHeartbeatService {
     
-    @JsonProperty("device_id")
-    private String deviceId;
+    private static final Logger logger = LoggerFactory.getLogger(ServerHeartbeatService.class);
     
-    @JsonProperty("server_status")
-    private String serverStatus;
-    
-    @JsonProperty("timestamp_received")
-    private Long timestampReceived;
-    
-    @JsonProperty("message")
-    private String message;
+    public ServerHeartbeatResponse processHeartbeat(ServerHeartbeat heartbeat) {
+        logger.debug("Processing server heartbeat from device: {} - Online: {}, Queue: {}", 
+                    heartbeat.getDeviceId(), heartbeat.getIsOnline(), heartbeat.getQueueSize());
+        
+        long timestampReceived = Instant.now().getEpochSecond();
+        
+        // Log heartbeat information
+        if (!Boolean.TRUE.equals(heartbeat.getIsOnline())) {
+            logger.warn("Device {} reports offline status", heartbeat.getDeviceId());
+        }
+        
+        if (heartbeat.getQueueSize() != null && heartbeat.getQueueSize() > 100) {
+            logger.warn("Device {} has high queue size: {}", 
+                       heartbeat.getDeviceId(), heartbeat.getQueueSize());
+        }
+        
+        // Create response
+        ServerHeartbeatResponse response = new ServerHeartbeatResponse();
+        response.setDeviceId(heartbeat.getDeviceId());
+        response.setServerStatus("online");
+        response.setTimestampReceived(timestampReceived);
+        response.setMessage("Heartbeat received successfully");
+        
+        // In a production system, you might want to:
+        // - Update device status in database
+        // - Alert if device is offline
+        // - Monitor queue sizes
+        // - Track uptime statistics
+        
+        return response;
+    }
 }
