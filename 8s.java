311e474a02d5c3817279722c12cb46f8d5611114
Reena246 <vrasagna7@gmail.com>
2026-01-19 18:10:19 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -3,14 +3,11 @@ package com.company.badgemate.dto;
 import lombok.Data;
 
 @Data
-public class EventLog {
-    private String eventId;
-    private String eventType; // card_scan, system_event, access_granted, access_denied
-    private Long doorId;
-    private String cardHex;
-    private String userName;
-    private String details;
-    private Long timestamp;
+public class ServerHeartbeat {
     private String deviceId;
-    private Long companyId;
+    private Long timestamp;
+    private Boolean isOnline;
+    private Integer queueSize;
+    private String dbVersionHash;
+    private Integer uptimeSeconds;
 }
