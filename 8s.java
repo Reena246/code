2f2146bde2bfae3228698646920cc1d30fced945
Reena246 <vrasagna7@gmail.com>
2026-01-19 18:20:58 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,9 +1,9 @@
 package com.company.badgemate.repository;
 
-import com.company.badgemate.entity.Building;
+import com.company.badgemate.entity.CardProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BuildingRepository extends JpaRepository<Building, Long> {
+public interface CardProviderRepository extends JpaRepository<CardProvider, Long> {
 }
