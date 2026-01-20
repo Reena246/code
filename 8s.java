04c3df0a147f912379aceffc368eac68f40e8931
Reena246 <vrasagna7@gmail.com>
2026-01-19 18:19:36 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,32 +1,9 @@
-package com.project.accesscontrol.config;
+package com.company.badgemate.repository;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.company.badgemate.entity.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-@Configuration
-@SecurityScheme(
-    name = "basicAuth",
-    type = SecuritySchemeType.HTTP,
-    scheme = "basic"
-)
-public class SwaggerConfig {
-    
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-            .info(new Info()
-                .title("BadgeMate Access Control API")
-                .version("1.0.0")
-                .description("REST API for real-time access control system with card validation and audit logging")
-                .contact(new Contact()
-                    .name("BadgeMate Support")
-                    .email("support@badgemate.com")
-                )
-            );
-    }
+@Repository
+public interface BuildingRepository extends JpaRepository<Building, Long> {
 }
