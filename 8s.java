29276a05690f4832434811f10dea1ec505badbcb
Reena246 <vrasagna7@gmail.com>
2026-01-19 18:00:36 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,12 +1,16 @@
 package com.company.badgemate.repository;
 
-import com.company.badgemate.entity.AccessGroupDoor;
+import com.company.badgemate.entity.Audit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface AccessGroupDoorRepository extends JpaRepository<AccessGroupDoor, AccessGroupDoor.AccessGroupDoorId> {
-    List<AccessGroupDoor> findByIdAccessGroupIdAndIdDoorIdAndIsActiveTrue(Long accessGroupId, Long doorId);
-    List<AccessGroupDoor> findByIdAccessGroupIdAndIsActiveTrue(Long accessGroupId);
+public interface AuditRepository extends JpaRepository<Audit, Long> {
+    @Query("SELECT AVG(a.openSeconds) FROM Audit a WHERE a.doorId = :doorId AND a.openSeconds IS NOT NULL")
+    Double findAverageOpenSecondsByDoorId(@Param("doorId") Long doorId);
+    
+    List<Audit> findByCardIdAndDoorIdOrderByEventTimeDesc(Long cardId, Long doorId);
 }
