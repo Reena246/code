815c65895f84271a6d9d375531f2fe282a6d62a0
Reena246 <vrasagna7@gmail.com>
2026-01-25 11:14:46 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,1 +1,16 @@
+INSERT INTO reader (
+    controller_id,
+    door_id,
+    reader_code,
+    reader_model,
+    is_active,
+    created,
+    updated,
+    created_by,
+    updated_by,
+    reader_uuid
+)
+VALUES
+(1, 1, 'ITSR-DOOR-1', 'HID-RP40', 1, NOW(), NOW(), 'system', 'system', UUID()),
+(1, 2, 'ITSR-DOOR-2', 'HID-RP40', 1, NOW(), NOW(), 'system', 'system', UUID());
 
