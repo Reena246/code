cf49f0e309688ed08cae2ffc07183b913af1d398
Reena246 <vrasagna7@gmail.com>
2026-01-25 22:43:01 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -4,22 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class BulkEventLogsRequest {
-    private String controllerMac;
-    private List<EventLog> events;
-
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class EventLog {
-        private String readerUuid;
-        private String cardHex;
-        private String eventType;
-        private String eventTime;
-    }
+public class BulkEventLogsResponse {
+    private String status;  // RECEIVED
+    private Integer processedCount;
 }
