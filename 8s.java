82aa73472ace50a97a2519571e65eb78be874f5b
Reena246 <vrasagna7@gmail.com>
2026-01-25 22:37:43 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,26 +1,15 @@
-package com.accesscontrol.repository;
+package com.accesscontrol.dto;
 
-import com.accesscontrol.entity.Audit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-@Repository
-public interface AuditRepository extends JpaRepository<Audit, Long> {
-    
-    List<Audit> findByControllerMacAndEventTimeBetween(
-            String controllerMac, 
-            LocalDateTime startTime, 
-            LocalDateTime endTime);
-    
-    @Query("SELECT AVG(a.openSeconds) FROM Audit a WHERE a.doorId = :doorId " +
-           "AND a.openSeconds IS NOT NULL AND a.eventTime > :sinceTime")
-    Double findAverageOpenSecondsByDoorId(@Param("doorId") Long doorId, 
-                                           @Param("sinceTime") LocalDateTime sinceTime);
-    
-    List<Audit> findByEmployeePkOrderByEventTimeDesc(Long employeePk);
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidateAccessRequest {
+    private String controllerMac;
+    private String readerUuid;
+    private String cardHex;
+    private String timestamp;
 }
