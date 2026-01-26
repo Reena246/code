cbf5dbe3fa0c359f084a48dee6182fe136260a6a
Reena246 <vrasagna7@gmail.com>
2026-01-25 22:31:40 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,22 +1,12 @@
 package com.accesscontrol.repository;
 
-import com.accesscontrol.entity.AccessGroupDoor;
+import com.accesscontrol.entity.Door;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface AccessGroupDoorRepository extends JpaRepository<AccessGroupDoor, AccessGroupDoor.AccessGroupDoorId> {
-    
-    @Query("SELECT agd FROM AccessGroupDoor agd WHERE agd.accessGroupId = :accessGroupId " +
-           "AND agd.doorId = :doorId AND agd.isActive = true")
-    Optional<AccessGroupDoor> findByAccessGroupIdAndDoorIdAndIsActive(
-            @Param("accessGroupId") Long accessGroupId, 
-            @Param("doorId") Long doorId);
-    
-    List<AccessGroupDoor> findByAccessGroupIdAndIsActive(Long accessGroupId, Boolean isActive);
+public interface DoorRepository extends JpaRepository<Door, Long> {
+    Optional<Door> findByDoorIdAndIsActive(Long doorId, Boolean isActive);
 }
