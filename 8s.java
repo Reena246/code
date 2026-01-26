8a0ad66e594cb7a85215cf7ab3970b37e405a2dc
Reena246 <vrasagna7@gmail.com>
2026-01-25 22:41:25 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -4,9 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class DbSyncRequest {
-    private String controllerMac;
+public class DbSyncResponse {
+    private List<ReaderSync> readers;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReaderSync {
+        private String readerUuid;
+        private List<String> allowedCards;
+    }
 }
