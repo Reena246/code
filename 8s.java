e2ccdd7acddd9bbefeec19907480d4be42bd7074
Reena246 <vrasagna7@gmail.com>
2026-01-25 22:27:43 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,7 +1,10 @@
-import com.accesscontrol.entity.AccessGroup;
+import com.accesscontrol.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AccessGroupRepository extends JpaRepository<AccessGroup, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmployeePkAndIsActive(Long employeePk, Boolean isActive);
 }
