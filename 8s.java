d373e4f5edebadf874150b7fc16310aacbcce698
Reena246 <vrasagna7@gmail.com>
2026-01-25 22:22:17 -0600

    Update 8s.java

--- 8s.java
+++ 8s.java
@@ -1,12 +1,7 @@
-package com.accesscontrol.repository;
-
-import com.accesscontrol.entity.Company;
+mport com.accesscontrol.entity.Site;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-    Optional<Company> findByCompanyIdAndIsActive(Long companyId, Boolean isActive);
+public interface SiteRepository extends JpaRepository<Site, Long> {
 }
